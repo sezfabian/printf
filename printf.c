1d345e734bc7921e968c299da161ee7ff03781f6
sezfabian <cheruiyotfabian@gmail.com>
2022-11-24 01:19:23 +0300

    correction 1

--- printf.c
+++ printf.c
@@ -10,7 +10,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	unsigned int c = 0, c1;
+	unsigned int c = 0, c1, cp = 0;
 	char *str;
 
 	va_start(args, format);
@@ -23,14 +23,21 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 				_putchar(va_arg(args, int));
+				cp++;
 				break;
 
 				case 's':
 				str = va_arg(args, char*);
+				if (str != 0)
+				{
 				for (c1 = 0; str[c1] != '\0'; c1++)
 				{
 					_putchar(str[c1]);
+					cp++;
 				}
+				}
+				else
+					cp = cp + _printf("(null)");
 				break;
 
 				case 'i':
@@ -42,9 +49,12 @@ int _printf(const char *format, ...)
 			}
 		}
 		else
-		_putchar(format[c]);
+		{
+			_putchar(format[c]);
+			cp++;
+		}
 		c++;
 	}
 	va_end(args);
-	return (0);
+	return (cp);
 }
