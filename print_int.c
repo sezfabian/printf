33beab676420e502f31ac8a0e761ef7d9f26b52c
mutugi001 <collinsmut04@gmail.com>
2022-11-24 00:13:49 +0300

    int file fix

--- print_int.c
+++ print_int.c
@@ -2,16 +2,17 @@
 /**
  * print_int- prints integer
  * @n: integer to print
+ *
  */
 
 void print_int(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-        print_int(n / 10);
-    _putchar((n % 10) + '0');
+	if (n < 0)
+    	{
+		_putchar('-');
+        	n = -n;
+	}
+	if (n / 10 != 0)
+		print_int(n / 10);
+	_putchar((n % 10) + '0');
 }
