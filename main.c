3798e0fabbf1cd82667388f563f05ac949a0204a
mutugi001 <collinsmut04@gmail.com>
2022-11-24 00:08:45 +0300

    fix

--- main.c
+++ main.c
@@ -11,8 +11,10 @@
 int main(void)
 {
 	int b = -53;
+
 	_printf("My name is: %s  %s age : %i \n", "fab", "cjer", b);
 	_printf("lets try\n");
 	_printf("%d\n", 3.14);
 	_printf("%u\n", b);
+	return (0);
 }
--- print_double.c
+++ print_double.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ * print_double- prints a double
+ * @f:the number
+ */
 
-void print_double( double f)
+void print_double(double f)
 {
 	int dec = 1000000;
 	int i = (int)f;
+
 	print_int(i);
 	f = (f - i) * dec;
 	i = (int)f;
